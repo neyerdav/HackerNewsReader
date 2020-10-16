80f6a35d5b3fab5c81fd8a577c042ab950271745
David Neyer <neyer.david@googlemail.com>
2020-10-16 11:40:52 +0200

    Add listView

--- app/src/main/java/dev/neyerdav/hackernewsreader/MainActivity.java
+++ app/src/main/java/dev/neyerdav/hackernewsreader/MainActivity.java
@@ -3,12 +3,23 @@ package dev.neyerdav.hackernewsreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+        listView.setAdapter(arrayAdapter);
     }
 }
